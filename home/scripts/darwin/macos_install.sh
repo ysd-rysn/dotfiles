f3650578b892acabfcf829d7d46930e8f89bd086
ysd-rysn <81076133+ysd-rysn@users.noreply.github.com>
2023-12-02 04:00:58 +0900

    update Brewfile

--- home/scripts/darwin/macos_install.sh
+++ home/scripts/darwin/macos_install.sh
@@ -5,7 +5,7 @@ if [ "$(uname)" != "Darwin" ]; then
 	exit 1
 fi
 
-BREWFILE="$HOME/dotfiles/home/Brewfile"
+BREWFILE="$HOME/dotfiles/home/dot_Brewfile"
 if [ -f "$BREWFILE" ]; then
 	softwareupdate --install-rosetta --agree-to-license
 	brew bundle --file="$BREWFILE"
