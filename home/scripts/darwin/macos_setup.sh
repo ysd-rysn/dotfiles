a56e589c6546c48b36deef460f29082c3ab19b84
ysd-rysn <81076133+ysd-rysn@users.noreply.github.com>
2023-12-02 03:12:17 +0900

    Update setup script

--- home/scripts/darwin/macos_setup.sh
+++ home/scripts/darwin/macos_setup.sh
@@ -5,8 +5,10 @@ if [ "$(uname)" != "Darwin" ]; then
 	exit 1
 fi
 
-# Install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if ! command -v brew >/dev/null; then
+	# Install homebrew
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
 
 # Install awscli, gh
@@ -17,5 +19,6 @@ brew install gh
 aws configure --profile chezmoi
 export AWS_PROFILE=chezmoi
 
-$HOME/dotfiles/home/scripts/macos_install.sh
-$HOME/dotfiles/home/scripts/macos_defaults.sh
+SCRIPT_DIR=$HOME/dotfiles/home/scripts/darwin
+$SCRIPT_DIR/macos_install.sh
+$SCRIPT_DIR/macos_defaults.sh
