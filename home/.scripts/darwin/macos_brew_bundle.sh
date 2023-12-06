7b6eb146efcaf966a8a7a8b50d11ad95ee11c14e
ysd-rysn <81076133+ysd-rysn@users.noreply.github.com>
2023-12-07 02:18:41 +0900

    update setup scripts

--- home/.scripts/darwin/macos_brew_bundle.sh
+++ home/.scripts/darwin/macos_brew_bundle.sh
@@ -7,7 +7,9 @@ fi
 
 BREWFILE="$HOME/.Brewfile"
 if [ -f "$BREWFILE" ]; then
+	echo "Install rosetta ..."
 	softwareupdate --install-rosetta --agree-to-license
+	echo "Install software in Brewfile  ..."
 	brew bundle --file="$BREWFILE"
 else
 	echo "Not found $BREWFILE"
