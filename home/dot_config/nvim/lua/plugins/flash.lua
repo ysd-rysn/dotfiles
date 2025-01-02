9f4472119d01d8d6b4474942421e76dfe77d9afe
ysd-rysn <81076133+ysd-rysn@users.noreply.github.com>
2025-01-02 15:50:59 +0900

    update vscode config

--- home/dot_config/nvim/lua/plugins/flash.lua
+++ home/dot_config/nvim/lua/plugins/flash.lua
@@ -1,8 +1,16 @@
 return {
-    "folke/flash.nvim",
-    keys = {
-        -- disable the default flash keymap
-        { "s",    mode = { "n", "x", "o" }, false },
-        { "<CR>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+  "folke/flash.nvim",
+  keys = {
+    -- disable the default flash keymap
+    { "s", mode = { "n", "x", "o" }, false },
+    { "S", mode = { "n", "x", "o" }, false },
+    {
+      "<CR>",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash",
     },
+  },
 }
